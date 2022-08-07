d00080f29c5f7bff5b930c815295d9f3dbb031fd
evanhyde <evanhydec@163.com>
2022-08-07 14:50:51 +0800

    最终版本

--- src/main/java/com/example/site/SiteApplication.java
+++ src/main/java/com/example/site/SiteApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cache.annotation.EnableCaching;
 @SpringBootApplication
 @EnableCaching
 public class SiteApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SiteApplication.class, args);
     }
